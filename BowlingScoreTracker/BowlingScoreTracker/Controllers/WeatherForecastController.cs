3b46ef703e20eb618d5bb864af246d711dc1d066
Arthur san Juan <arthur.sanjuan@outlook.ph>
2022-06-25 12:43:24 +0800

    Update WeatherForecastController.cs

--- BowlingScoreTracker/BowlingScoreTracker/Controllers/WeatherForecastController.cs
+++ BowlingScoreTracker/BowlingScoreTracker/Controllers/WeatherForecastController.cs
@@ -21,6 +21,8 @@ namespace BowlingScoreTracker.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+
+            //Test comment
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
